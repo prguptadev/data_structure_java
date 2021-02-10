a9a2ccecd4864140163e537b61bcf635026cb0bd
Prashant Gupta <pr.gupta1993@gmail.com>
2021-02-10 23:28:39 +0530

    initial commit

--- src/com/prguptadev/Main.java
+++ src/com/prguptadev/Main.java
@@ -3,6 +3,18 @@ package com.prguptadev;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] array = new int[7];
+	    array[0] = 20;
+	    array[1] = 10;
+        array[2] = -20;
+        array[3] = 90;
+        array[4] = 20;
+        array[5] = -10;
+        array[6] = 20;
+       // array[7] = 40; java.lang.ArrayIndexOutOfBoundsException:
+
+        for(int i=0; i<array.length; i++)
+            System.out.println(array[i]);
+
     }
 }
